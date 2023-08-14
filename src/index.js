aaf6e16e79b2e27e3c1ab5d192391e93e0823469
krlinh <dlinhks94@gmail.com>
2023-08-14 16:33:31 +0700

    Build and css header, custom grid for project

--- src/index.js
+++ src/index.js
@@ -16,9 +16,6 @@ app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources','views'));
 
-// app.get('/', (req, res) => {
-//   res.render('home')
-// })
 // Route init
 route(app)
 app.listen(port, () => {
