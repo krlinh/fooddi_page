49dd2e173fad3bea7c1dcc251b94a6265b98bc02
krlinh <dlinhks94@gmail.com>
2023-10-12 15:30:11 +0700

    Install method-override and in import it in index.js file.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var methodOverride = require('method-override');
 const morgan = require('morgan');
 const app = express();
 const port = 3500;
@@ -9,9 +10,9 @@ const dbconnect = require('./config/db');
 const bodyParser = require('body-parser');
 
 app.use(morgan('combined'));
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 
 // connect to db
 dbconnect.connect();
